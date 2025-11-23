688be04fac30f7f530a89d9ace969b04693462d4
Duncan Squires <dsdevnull@gmail.com>
2025-11-23 10:24:04 -0500

    WIP

--- modelwork/create_model.py
+++ modelwork/create_model.py
@@ -1,4 +1,6 @@
 import tarfile
+from typing import Tuple
+
 import requests
 import os
 import polars as pl
@@ -7,6 +9,7 @@ from multiprocessing import Pool, cpu_count
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 # setting up new Stemmer and Tokenizer
 tokenizer = TreebankWordTokenizer()
@@ -36,7 +39,7 @@ def read_tar_gz(file_path: str):
         raise FileNotFoundError(f"{file_path} does not exist")
 
 
-def load_text_file(base_dir: str) -> pl.Dataframe:
+def load_text_file(base_dir: str) -> pl.DataFrame:
     base_dir = Path(base_dir)
     data = []
 
@@ -51,7 +54,7 @@ def load_text_file(base_dir: str) -> pl.Dataframe:
     return pl.DataFrame(data, schema=["review", "sentiment"])
 
 
-def preprocess_dataframe(raw_df: pl.Dataframe) -> pl.Dataframe:
+def preprocess_dataframe(raw_df: pl.DataFrame) -> pl.DataFrame:
     print("preprocessing...")
     cleaned = raw_df.with_columns(
         pl.col("review")
@@ -79,7 +82,7 @@ def stem_and_remove_stop_words(text: str) -> str:
     return " ".join(cleaned_tokens)
 
 
-def apply_stemmer_and_tokenizer(pre_df: pl.Dataframe) -> pl.Dataframe:
+def apply_stemmer_and_tokenizer(pre_df: pl.DataFrame) -> pl.DataFrame:
     print("stemming and removing stop words...")
     reviews = pre_df["review"].to_list()
 
@@ -90,16 +93,33 @@ def apply_stemmer_and_tokenizer(pre_df: pl.Dataframe) -> pl.Dataframe:
     return cleaned
 
 
-def split_test_and_train_data(whole_df: pl.Dataframe) -> pl.Dataframe:
-    norm_train_reviews = (
-        whole_df.lazy()
-        .select("review")
-        .limit(40000)
-        .collect()
-    )
+def split_test_and_train_data(whole_df: pl.DataFrame) -> Tuple[pl.Series, pl.Series]:
+    rounded_num_rows = int(round(len(whole_df) * 0.8))
+    train = whole_df["review"][:rounded_num_rows]
+    test = whole_df["review"][rounded_num_rows:]
+    return train, test
 
+def count_vectorizer(train_df: pl.Series, test_df: pl.Series) -> Tuple[pl.Series, pl.Series]:
+    cv = CountVectorizer(min_df=0.0, max_df=1.0, binary=False, ngram_range=(1, 3))
+    # transformed train reviews
+    cv_train_reviews = cv.fit_transform(train_df)
+    # transformed test reviews
+    cv_test_reviews = cv.transform(test_df)
+
+    print("BOW_cv_train:", cv_train_reviews.shape)
+    print("BOW_cv_test:", cv_test_reviews.shape)
+    return cv_train_reviews, cv_test_reviews
+
+def term_freq_inverse_document_freq(train_df: pl.Series, test_df: pl.Series) -> Tuple[pl.Series, pl.Series]:
+    tv = TfidfVectorizer(min_df=0.0, max_df=1.0, use_idf=True, ngram_range=(1, 3))
+    tv_train_reviews = tv.fit_transform(train_df)
+    tv_test_reviews = tv.transform(test_df)
+    print("Tfidf_train:", tv_train_reviews.shape)
+    print("Tfidf_test:", tv_test_reviews.shape)
+    return tv_train_reviews, tv_test_reviews
 
 if __name__ == "__main__":
+    #Have this be a flag that can be sent from the api
     development_flag = True
     if development_flag:
         df = pl.read_csv("./data/output.csv")
@@ -114,3 +134,8 @@ if __name__ == "__main__":
 
     cleaned_df = preprocess_dataframe(raw_df=df)
     cleaned_df = apply_stemmer_and_tokenizer(cleaned_df)
+    train_data, test_data = split_test_and_train_data(cleaned_df)
+    cv_train_data, cv_test_data = count_vectorizer(train_data, test_data)
+    tv_train_data, tv_test_data = term_freq_inverse_document_freq(train_data, test_data)
+    print(tv_train_data.shape)
+    print(tv_test_data.shape)
